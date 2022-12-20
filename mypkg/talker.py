706ac69d26b836b1da720404e521711b304d73cd
root <root@DESKTOP-V0CH8E9.localdomain>
2022-12-20 14:28:17 +0900

    add a command

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -2,16 +2,28 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int16
 
+class Talker():
+    def __init__(self):
+        self.pub = node.create_publisher(Int16,"countup", 10)
+        self.n = 0
+        node.create_timer(0.5, cd)
+
+    def cd(self):
+        msg = Int16()
+        msg.data = self.n
+        self.pub.publish(msg)
+        self.n += 1
+        
 rclpy.init()
 node = Node("talker")
-pub = node.create_publisher(Int16, "countup", 10)
-n = 0
+talker = Talker(node)
+rclpy.spin(node)
+
 def cd():
-    global n
     msg = Int16()
-    msg.data = n
-    pub.publish(msg)
-    n += 1
+    msg.data = talker.n
+    talker.pub.publish(msg)
+    talker.n += 1
 
 node.create_timer(0.5, cd)
 rclpy.spin(node)
